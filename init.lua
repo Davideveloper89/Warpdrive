276d8a34f6cb39cc2d59d6d65291ad85e5e00c7e
Neil Rizen <33267501+newrizen@users.noreply.github.com>
2026-05-22 14:20:14 -0300

    New wormhole portal form and behavior

--- init.lua
+++ init.lua
@@ -2,39 +2,58 @@ local function xyz(fx, fy, fz) return { x = fx, y = fy, z = fz } end
 local c = core
 local wormhole_portals = {} -- Armazena os portais colocados
 -- Bloco do portal normal
-c.register_node("warpdrive:wormhole", {
+c.register_node("nh_nodes:wormhole", {
 	description = "Wormhole Portal",
-	tiles = { "wormhole.png" },
+	drawtype = "nodebox",
+	tiles = {
+		"wormhole.png",   -- topo/baixo
+		"wormhole.png", -- Se quiser, use aqui uma imagem de animação própria, para frente/trás do nodebox (efeito portal)
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	walkable = false,       -- jogador atravessa
+	pointable = true,
+	node_box = {type = "fixed", fixed = {-0.5, -0.5, -0.02, 0.5, 0.5, 0.02},}, -- plano fino
+	selection_box = {type = "fixed", fixed = {-0.5, -0.5, -0.02, 0.5, 0.5, 0.02},},
 	light_source = 14,
 	walkable = false,
 	pointable = true,
-	groups = { cracky = 1 },
+	groups = {cracky = 1},
 	on_construct = function(pos)
 		-- Adiciona o portal à lista
 		table.insert(wormhole_portals, pos)
-		if #wormhole_portals == 2 then c.chat_send_all "Os portais foram conectados!" end -- Se houver dois portais, conectá-los
+		if #wormhole_portals == 2 then c.chat_send_all("Os portais foram conectados!") end -- Se houver dois portais, conectá-los
 		-- Efeito de partículas
 		c.add_particlespawner({
-			amount = 100,
+			amount = 50,
 			time = 0,
-			minpos = vector.subtract(pos, 1),
-			maxpos = vector.add(pos, 1),
-			minvel = xyz(-1, 0, -1),
-			maxvel = xyz(1, 1, 1),
-			texture = "wormhole_particle.png",
+			minpos = vector.subtract(pos, 0.5),
+			maxpos = vector.add(pos, 0.5),
+			minvel = xyz(-0.5, -0.5, -0.5),
+			maxvel = xyz(0.5, 0.5, 0.5),
+			minsize = 0.5,
+        		maxsize = 1,
+			texture = "spark_particle.png^[colorize:#000000:255", -- opacidade completa de pintura sobre textura: 255 - hexa pra azul: #028dde
 			glow = 10
 		})
 	end,
 	on_destruct = function(pos)
 		for _, p in ipairs(wormhole_portals) do -- Remove o portal da lista ao ser destruído
-			if vector.equals(p, pos) then table.remove(wormhole_portals, _) break end
+			if vector.equals(p, pos) then
+				table.remove(wormhole_portals, _)
+				break
+			end
 		end
 	end,
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-		if #wormhole_portals < 2 then c.chat_send_player(player:get_player_name(), "O portal ainda não está conectado!") return end
+		if #wormhole_portals < 2 then
+			c.chat_send_player(player:get_player_name(), "O portal ainda não está conectado!")
+			return
+		end
 		-- Descobre qual portal é o outro
 		local target_pos = wormhole_portals[1]
-		target_pos = vector.equals(pos, wormhole_portals[1]) and wormhole_portals[2] or target_pos
+		if vector.equals(pos, wormhole_portals[1]) then target_pos = wormhole_portals[2] end
 		-- Teletransporta o jogador
 		player:set_pos(target_pos)
 		c.chat_send_player(player:get_player_name(), "Você entrou no buraco de minhoca!")
